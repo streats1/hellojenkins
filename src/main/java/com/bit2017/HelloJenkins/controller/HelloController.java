8983683beebd3a6cfffa899cc6215b3242295f93
streats <streats99@gmail.com>
2017-03-15 11:44:48 +0900

    1

---
+++ src/main/java/com/bit2017/HelloJenkins/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.bit2017.HelloJenkins.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@ResponseBody
+	@RequestMapping("/hello")
+	public String Hello(){
+		return "helloJenKins";
+	}
+
+}
